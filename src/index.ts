d7cddbd21826144e360417a9fd2c3f370542f260
Fadenfire <13701628+Fadenfire@users.noreply.github.com>
2022-04-02 18:05:03 -0500

    Remove inlined files from the bundle

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,6 @@ export function viteSingleFile(): Plugin {
 				// Only use this plugin during build
 				if (!ctx || !ctx.bundle) return html
 				// Get the bundle
-				let extraCode = ""
 				for (const [, value] of Object.entries(ctx.bundle)) {
 					const o = value as OutputChunk
 					const a = value as OutputAsset
@@ -20,15 +19,17 @@ export function viteSingleFile(): Plugin {
 						const reScript = new RegExp(`<script type="module"[^>]*?src="[\./]*${o.fileName}"[^>]*?></script>`)
 						const code = `<script type="module">\n//${o.fileName}\n${o.code}\n</script>`
 						html = html.replace(reScript, (_) => code)
+						delete ctx.bundle[o.fileName]
 					} else if (a.fileName.endsWith(".css")) {
 						const reCSS = new RegExp(`<link rel="stylesheet"[^>]*?href="[\./]*${a.fileName}"[^>]*?>`)
 						const code = `<style type="text/css">\n${a.source}\n</style>`
 						html = html.replace(reCSS, (_) => code)
+						delete ctx.bundle[a.fileName]
 					} else {
 						console.warn(`${chalk.yellow("WARN")} asset not inlined: ${chalk.green(a.fileName)}`)
 					}
 				}
-				return html.replace(/<\/body>/, extraCode + "</body>")
+				return html
 			},
 		},
 	}
