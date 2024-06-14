7a1af060f96e88df4c859c2ccb21d68c570541b9
Gnohz Gniy <0x00eeee@gmail.com>
2024-06-14 15:31:41 +0800

    Fix __VITE_PRELOAD__ regression in vite 5.3

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ const defaultConfig = { useRecommendedBuildConfig: true, removeViteModuleLoader:
 export function replaceScript(html: string, scriptFilename: string, scriptCode: string, removeViteModuleLoader = false): string {
 	const reScript = new RegExp(`<script([^>]*?) src="[./]*${scriptFilename}"([^>]*)></script>`)
 	// we can't use String.prototype.replaceAll since it isn't supported in Node.JS 14
-	const preloadMarker = /"__VITE_PRELOAD__"/g
+	const preloadMarker = /"?__VITE_PRELOAD__"?/g
 	const newCode = scriptCode.replace(preloadMarker, "void 0")
 	const inlined = html.replace(reScript, (_, beforeSrc, afterSrc) => `<script${beforeSrc}${afterSrc}>${newCode}</script>`)
 	return removeViteModuleLoader ? _removeViteModuleLoader(inlined) : inlined
